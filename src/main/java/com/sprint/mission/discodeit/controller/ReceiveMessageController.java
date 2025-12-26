9dde5d525df86c87b1c4e00802eb13ba38ebd86b
yeeun000 <dldpdms0000@naver.com>
2025-12-26 23:33:37 +0900

    메시지 수신 관리 구현

--- src/main/java/com/sprint/mission/discodeit/controller/ReceiveMessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReceiveMessageController.java
@@ -1,7 +1,34 @@
 package com.sprint.mission.discodeit.controller;
 
-import org.springframework.stereotype.Controller;
+import com.sprint.mission.discodeit.dto.readStatusDTO.ReadStatusDTO;
+import com.sprint.mission.discodeit.dto.readStatusDTO.UpdateReadStatusRequest;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.service.ReadStatusService;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.UUID;
+
+@RestController
 public class ReceiveMessageController {
+
+    private final ReadStatusService readStatusService;
+
+    public ReceiveMessageController(ReadStatusService readStatusService) {
+        this.readStatusService = readStatusService;
+    }
+
+    @RequestMapping(value = "/receiveMessage", method = RequestMethod.POST)
+    public ReadStatus createReceiveMessage(@RequestBody ReadStatusDTO readStatusDTO) {
+        return readStatusService.create(readStatusDTO);
+    }
+
+    @RequestMapping(value = "/receiveMessage", method = RequestMethod.PUT)
+    public void updateReceiveMessage(@RequestBody UpdateReadStatusRequest updateReadStatusRequest) {
+        readStatusService.update(updateReadStatusRequest);
+    }
+
+    @RequestMapping(value = "/receiveMessage", method = RequestMethod.GET)
+    public void userReceiveMessage(@RequestParam UUID userId) {
+        readStatusService.findAllByUserId(userId);
+    }
 }
