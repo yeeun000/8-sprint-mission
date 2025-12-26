5fe3d54ba73b7b44ab4094f6b4aaf10b32a85e91
yeeun000 <dldpdms0000@naver.com>
2025-12-26 23:41:48 +0900

    파일 조회

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,8 +1,27 @@
 package com.sprint.mission.discodeit.controller;
 
 
-import org.springframework.stereotype.Controller;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.UUID;
+
+@RestController
 public class BinaryContentController {
+
+    private final BinaryContentService binaryContentService;
+
+    public BinaryContentController(BinaryContentService binaryContentService) {
+        this.binaryContentService = binaryContentService;
+    }
+
+    @RequestMapping(value = "/file", method = RequestMethod.GET)
+    public List<BinaryContent> findAllByIdIn(@RequestParam List<UUID> userId) {
+        return binaryContentService.findAllByIdIn(userId);
+    }
 }
