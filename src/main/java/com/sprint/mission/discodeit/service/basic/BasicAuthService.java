c513f5c626bcb5af4659ebbd2deac2b6c8d0bef5
yeeun000 <dldpdms0000@naver.com>
2025-12-31 11:21:12 +0900

    refactor : AuthController 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,31 +1,29 @@
 package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.LoginRequest;
-import com.sprint.mission.discodeit.dto.LoginResponse;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
 
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    @Override
-    public LoginResponse login(LoginRequest loginRequest) {
-        User user = userRepository.findByUsername(loginRequest.username())
-                .orElseThrow(() -> new NoSuchElementException("아이디가 틀렸습니다."));
+  @Override
+  public User login(LoginRequest loginRequest) {
+    User user = userRepository.findByUsername(loginRequest.username())
+        .orElseThrow(() -> new NoSuchElementException("아이디가 틀렸습니다."));
 
-        if (loginRequest.password().equals(user.getPassword())){
-            LoginResponse response = new LoginResponse(user.getId(), user.getName());
-            return response;
-        }
-        else throw new NoSuchElementException("비밀번호가 틀렸습니다.");
+    if (loginRequest.password().equals(user.getPassword())) {
+      return user;
+    } else {
+      throw new NoSuchElementException("비밀번호가 틀렸습니다.");
     }
+  }
 
 }
