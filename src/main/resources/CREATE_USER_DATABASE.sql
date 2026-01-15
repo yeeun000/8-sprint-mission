124f1476edbcb26a86405c469c2a27f22385717e
yeeun000 <dldpdms0000@naver.com>
2026-01-15 14:34:29 +0900

    feat : db 테이블 작성

--- src/main/resources/CREATE_USER_DATABASE.sql
+++ src/main/resources/CREATE_USER_DATABASE.sql
@@ -6,25 +6,20 @@ SELECT rolname, rolsuper, rolcreatedb
 FROM pg_roles
 WHERE rolname = 'discodeit_user';
 
--- 2) 데이터베이스 생성 후 스키마 생성
--- 데이터베이스를 생성한다
-CREATE DATABASE discodeitdb
+CREATE DATABASE discodeit
     WITH
     OWNER = discodeit_user
     ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF8'
-    LC_CTYPE = 'en_US.UTF8'
-    TEMPLATE template0;
 
-\c discodeitdb
+\c discodeit
 
-CREATE SCHEMA IF NOT EXISTS discodeitdb;
+CREATE SCHEMA IF NOT EXISTS discodeit;
+GRANT ALL PRIVILEGES ON SCHEMA discodeit TO discodeit_user;
 
--- 스키마 사용 권한을 부여한다
-GRANT ALL PRIVILEGES ON SCHEMA discodeitdb TO discodeit_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA discodeitdb TO discodeit_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA discodeit GRANT ALL ON TABLES TO discodeit_user;
 
--- 권한을 확인한다
+
+
 SELECT grantor,
        grantee,
        table_schema,
@@ -32,8 +27,5 @@ SELECT grantor,
 FROM information_schema.role_table_grants
 WHERE grantee = 'discodeit_user';
 
--- 3) 기본 검색 경로에 새로운 스키마를 추가한다
-ALTER ROLE ohgiraffers SET search_path TO menudb, public;
-
--- 현재 세션의 검색 경로를 확인한다
+ALTER ROLE discodeit_user SET search_path TO discodeit, public;
 SHOW search_path;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,83 +1,102 @@
--- 1. 테이블 삭제
-DROP TABLE IF EXISTS tbl_payment_order CASCADE;
-DROP TABLE IF EXISTS tbl_payment CASCADE;
-DROP TABLE IF EXISTS tbl_order_menu CASCADE;
-DROP TABLE IF EXISTS tbl_order CASCADE;
-DROP TABLE IF EXISTS tbl_menu CASCADE;
-DROP TABLE IF EXISTS tbl_category CASCADE;
+DROP TABLE IF EXISTS message_attachments CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS read_statuses CASCADE;
+DROP TABLE IF EXISTS user_statuses CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS channels CASCADE;
+DROP TABLE IF EXISTS binary_contents CASCADE;
 
--- 2. 테이블 생성
-CREATE TABLE IF NOT EXISTS users
-(
-    -- column level constraints
-    id         UUID PRIMARY KEY,
-    username   VARCHAR(50)  NOT NULL,
-    password   VARCHAR(60)  NOT NULL,
-    email      VARCHAR(100) NOT NULL,
-    profile_id UUID,
-    created_at TIMESTAMP    NOT NULL,
-    updated_at TIMESTAMP    NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS user_statues
+CREATE TABLE IF NOT EXISTS binary_contents
 (
-    id             UUID PRIMARY KEY,
-    user_id        UUID      NOT NULL,
-    last_active_at TIMESTAMP NOT NULL,
-    created_at     TIMESTAMP NOT NULL,
-    updated_at     TIMESTAMP NOT NULL,
-    CONSTRAINT fk_user_status_user FOREIGN KEY (user_id) REFERENCES users (id)
+    id           uuid,
+    created_at   timestamptz  NOT NULL,
+    file_name    varchar(255) NOT NULL,
+    size         bigint       NOT NULL,
+    content_type varchar(100) NOT NULL,
+    bytes        bytea        NOT NULL,
+
+    CONSTRAINT pk_binary_content_id PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS channels
 (
-    id          UUID PRIMARY KEY,
-    name        VARCHAR(100),
-    description VARCHAR(500),
-    type        VARCHAR(10) NOT NULL,
-    create_at   TIMESTAMP   NOT NULL,
-    update_at   TIMESTAMP   NOT NULL
+    id          uuid,
+    created_at  timestamptz NOT NULL,
+    updated_at  timestamptz,
+    name        varchar(100),
+    description varchar(500),
+    type        varchar(10) NOT NULL,
+
+    CONSTRAINT pk_channels_id PRIMARY KEY (id),
+    CONSTRAINT check_channel_type CHECK (type IN ('PUBLIC', 'PRIVATE'))
 );
 
-CREATE TABLE IF NOT EXISTS read_statues
+CREATE TABLE IF NOT EXISTS users
 (
-    id           UUID PRIMARY KEY,
-    user_id      UUID      NOT NULL,
-    channel_id   UUID      NOT NULL,
-    last_read_at TIMESTAMP NOT NULL,
-    created_at   TIMESTAMP NOT NULL,
-    updated_at   TIMESTAMP NOT NULL,
-    CONSTRAINT fk_read_status_user FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT fk_read_status_channel FOREIGN KEY (channel_id) REFERENCES channels (id)
+    id         uuid,
+    created_at timestamptz  NOT NULL,
+    updated_at timestamptz,
+    username   varchar(50)  NOT NULL,
+    email      varchar(100) NOT NULL,
+    password   varchar(60)  NOT NULL,
+    profile_id uuid,
+
+    CONSTRAINT pk_users_id PRIMARY KEY (id),
+    CONSTRAINT uk_users_username UNIQUE (username),
+    CONSTRAINT uk_users_email UNIQUE (email),
+    CONSTRAINT fk_users_profile_id FOREIGN KEY (profile_id) REFERENCES binary_contents (id) ON DELETE SET NULL,
+    CONSTRAINT uk_users_profile_id UNIQUE (profile_id)
 );
 
-CREATE TABLE IF NOT EXISTS binary_contents
+CREATE TABLE IF NOT EXISTS user_statuses
 (
-    id           UUID PRIMARY KEY,
-    file_name    VARCHAR(255) NOT NULL,
-    content_type VARCHAR(100) NOT NULL,
-    bytes        BYTEA        NOT NULL,
-    size         BIGINT       NOT NULL,
-    created_at   TIMESTAMP    NOT NULL
+    id             uuid,
+    created_at     timestamptz NOT NULL,
+    updated_at     timestamptz,
+    user_id        uuid        NOT NULL,
+    last_active_at timestamptz NOT NULL,
+
+    CONSTRAINT pk_user_statuses_id PRIMARY KEY (id),
+    CONSTRAINT fk_user_statuses_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT uk_user_statuses_user_id UNIQUE (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS read_statuses
+(
+    id           uuid,
+    created_at   timestamptz NOT NULL,
+    updated_at   timestamptz,
+    user_id      uuid        NOT NULL,
+    channel_id   uuid        NOT NULL,
+    last_read_at timestamptz NOT NULL,
+
+    CONSTRAINT pk_read_statuses_id PRIMARY KEY (id),
+    CONSTRAINT fk_read_statuses_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT uk_read_statuses_user_id UNIQUE (user_id, channel_id),
+    CONSTRAINT fk_read_statuses_channel_id FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS messages
 (
-    id         UUID PRIMARY KEY,
-    content    TEXT      NOT NULL,
-    channel_id UUID      NOT NULL,
-    author_id  UUID      NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    CONSTRAINT fk_message_channel FOREIGN KEY (channel_id) REFERENCES channels (id),
-    CONSTRAINT fk_message_author FOREIGN KEY (author_id) REFERENCES users (id)
+    id         uuid,
+    created_at timestamptz NOT NULL,
+    updated_at timestamptz,
+    content    text,
+    channel_id uuid        NOT NULL,
+    author_id  uuid,
+
+    CONSTRAINT pk_messages_id PRIMARY KEY (id),
+    CONSTRAINT fk_messages_channel_id FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
+    CONSTRAINT fk_messages_author_id FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS message_attachments
 (
-    message_id    UUID NOT NULL,
-    attachment_id UUID NOT NULL,
-    PRIMARY KEY (message_id, attachment_id),
-    CONSTRAINT fk_attachment_message FOREIGN KEY (message_id) REFERENCES messages (id),
-    CONSTRAINT fk_attachment_binary FOREIGN KEY (attachment_id) REFERENCES binary_contents (id)
+    message_id    uuid NOT NULL,
+    attachment_id uuid NOT NULL,
+
+    CONSTRAINT pk_message_attachments PRIMARY KEY (message_id, attachment_id),
+    CONSTRAINT fk_message_attachments_message_id FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
+    CONSTRAINT fk_message_attachments_attachment_id FOREIGN KEY (attachment_id) REFERENCES binary_contents (id) ON DELETE CASCADE
 );
