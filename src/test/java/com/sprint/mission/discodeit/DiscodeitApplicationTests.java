a99a51bc987d4de0255bf13b56dcd023a568854d
yeeun000 <dldpdms0000@naver.com>
2026-02-25 18:53:03 +0900

    deploy.yml

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -2,8 +2,10 @@ package com.sprint.mission.discodeit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DiscodeitApplicationTests {
 
 	@Test
