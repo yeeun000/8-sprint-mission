f4ac2e0f12a9f512641bffb1f0181261b8b8871e
yeeun000 <dldpdms0000@naver.com>
2026-02-26 10:49:59 +0900

    test 모범답안 변경

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class DiscodeitApplicationTests {
