0b0250f63363adf40a261038cb3c2cd2b5df9d9c
yeeun000 <dldpdms0000@naver.com>
2026-04-10 15:49:10 +0900

    refactor : admin 하드 코딩 수정

--- src/main/java/com/sprint/mission/discodeit/auth/AdminInitializer.java
+++ src/main/java/com/sprint/mission/discodeit/auth/AdminInitializer.java
@@ -5,6 +5,7 @@ import static com.sprint.mission.discodeit.entity.Role.ADMIN;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AdminInitializer implements CommandLineRunner {
 
+  @Value("${discodeit.admin.username}")
+  private String username;
+  @Value("${discodeit.admin.email}")
+  private String email;
+  @Value("${discodeit.admin.password}")
+  private String password;
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
   public void run(String... args) throws Exception {
     if (!userRepository.existsByRole(ADMIN)) {
-      User admin = new User("admin", "admin@admin.com", passwordEncoder.encode("admin"), null);
+      User admin = new User(username, email, passwordEncoder.encode(password), null);
       admin.setRole(ADMIN);
       userRepository.save(admin);
     }
