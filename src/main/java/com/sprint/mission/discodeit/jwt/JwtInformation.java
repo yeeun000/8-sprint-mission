0917da9ce142614fd11aa99bbd582b398327bf56
yeeun000 <dldpdms0000@naver.com>
2026-04-17 10:19:05 +0900

    fix : JwtInformation record로 변경

--- src/main/java/com/sprint/mission/discodeit/jwt/JwtInformation.java
+++ src/main/java/com/sprint/mission/discodeit/jwt/JwtInformation.java
@@ -1,26 +1,11 @@
 package com.sprint.mission.discodeit.jwt;
 
 import com.sprint.mission.discodeit.dto.data.UserDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class JwtInformation {
-
-  private UserDto userDto;
-  private String accessToken;
-  private String refreshToken;
-
-  public void rotate(String accessToken, String refreshToken) {
-    this.accessToken = accessToken;
-    this.refreshToken = refreshToken;
-  }
-
-  public void updateUser(UserDto userDto) {
-    this.userDto = userDto;
-  }
+public record JwtInformation(
+    UserDto userDto,
+    String accessToken,
+    String refreshToken
+) {
 
 }
