8022bc5c80563a2acb7f7edbc24b1c27602d8d87
yeeun000 <dldpdms0000@naver.com>
2026-04-23 14:25:35 +0900

    feat : Redis 도입

--- src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
@@ -1,31 +1,40 @@
 package com.sprint.mission.discodeit.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import java.time.Duration;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
   @Bean
-  public CacheManager cacheManager() {
-    CaffeineCacheManager cacheManager = new CaffeineCacheManager(
-        "users",
-        "channels",
-        "notifications"
+  public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+    ObjectMapper redisObjectMapper = objectMapper.copy();
+    redisObjectMapper.activateDefaultTyping(
+        LaissezFaireSubTypeValidator.instance,
+        DefaultTyping.EVERYTHING,
+        As.PROPERTY
     );
-    cacheManager.setCaffeine(Caffeine.newBuilder()
-        .maximumSize(1000)
-        .expireAfterWrite(Duration.ofMinutes(10))
-        .expireAfterAccess(Duration.ofMinutes(5))
-        .recordStats());
 
-    return cacheManager;
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer(redisObjectMapper)
+            )
+        )
+        .prefixCacheNameWith("discodeit:")
+        .entryTtl(Duration.ofSeconds(600))
+        .disableCachingNullValues();
   }
+
 
 }
