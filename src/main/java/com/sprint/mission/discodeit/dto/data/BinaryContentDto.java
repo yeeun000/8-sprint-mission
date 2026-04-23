15cfc2749cb461bffb174d800768a46cbad11ebc
yeeun000 <dldpdms0000@naver.com>
2026-04-23 09:51:03 +0900

    fix : 이미지 안 뜨는 문제 해결

--- src/main/java/com/sprint/mission/discodeit/dto/data/BinaryContentDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/BinaryContentDto.java
@@ -1,12 +1,14 @@
 package com.sprint.mission.discodeit.dto.data;
 
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
 import java.util.UUID;
 
 public record BinaryContentDto(
     UUID id,
     String fileName,
     Long size,
-    String contentType
+    String contentType,
+    BinaryContentStatus status
 ) {
 
 }
